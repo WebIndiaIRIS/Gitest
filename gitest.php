727bd1e12728d711035440016cec9e879deff310
WebIndiaIRIS <85987876+WebIndiaIRIS@users.noreply.github.com>
2021-06-16 15:08:24 +0530

    Create gitest.php

---
+++ gitest.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name: Gitest
+ * Plugin URI: https://iris.co.uk
+ * Description: Test plugin for git.
+ * Version: 1.0
+ * Author: David Wolfpaw
+ * License: GPL v3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: arch-wnb
+ * Domain Path: /languages
+*/
+?>
